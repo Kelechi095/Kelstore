1ab772f936e1704fec518672828202a5ba7fcd49
Awoms Kelechi <awomskelechi@gmail.com>
2023-12-19 18:25:53 +0100

    created and styled home page and mapped dummy products

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 
+import HomeBanner from "./components/HomeBanner";
+import Wrapper from "./components/Wrapper";
+import ProductCard from "./components/products/ProductCard";
+import { products } from "./utils/products";
+import { truncateText } from "./utils/truncateText";
+
 
 export default function Home() {
   return (
-    <div>
-      Main page
+    <div className="p-8">
+      <Wrapper>
+        <div>
+          <HomeBanner />
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl-grid-cols-5 2xl:grid-cols-6 gap-8">
+          {products.map((product: any) => (
+            <div key={product.id}>
+              <ProductCard data={product}/>
+            </div>
+          ))}
+        </div>
+      </Wrapper>
     </div>
   )
 }
